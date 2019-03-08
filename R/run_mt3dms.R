47c1db4d0c020d64b16c6c57c879d787e9d20da6
Alberto Casillas <ingcasillastrasvina@gmail.com>
2019-03-08 16:14:50 +0100

    Update run_mt3dms.R

--- R/run_mt3dms.R
+++ R/run_mt3dms.R
@@ -5,10 +5,9 @@
 #' @param file path to name file; typically '*.nam'
 #' @param mt3dms_executable name of the MT3DMS executable to use
 #' @export
-run_mt3dms <- function(file,mt3dms_executable='mt3dms5b')
+run_mt3dms <- function(file, mt3dms_executable='mt3dms5b')
 {
   dir <- dirname(file)
   file <- basename(file)
-  if(Sys.info()['sysname']=='Linux') system(paste('cd',dir,'&',modflow_executable,file))
-  if(Sys.info()['sysname']=='Windows') shell(paste('cd',dir,'&',modflow_executable,file),mustWork=TRUE)
+  shell(paste('cd',dir,'&',mt3dms_executable,file),mustWork=TRUE)
 }
